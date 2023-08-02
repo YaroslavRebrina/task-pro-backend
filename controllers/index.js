6c69f2bbf6f98542ba1e3d12370421a45216e776
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-08-02 22:49:36 +0300

    fix: path for changeUserSettings fixed

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const registration = require("./users/registration");
 const login = require("./users/login");
 // const logout = require("./users/logout");
-const changeUserSettings = require("./user/changeUserSettings");
+const changeUserSettings = require("./users/changeUserSettings");
 
-module.exports = { registration, login };
+module.exports = { registration, login, changeUserSettings };
