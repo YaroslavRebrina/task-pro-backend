1c41ed90e80c69639872b2392ee8bb105201c96c
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-08-03 15:02:11 +0300

    fix:res form

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -39,10 +39,8 @@ const login = async (req, res) => {
 
   res.status(200).json({
     token: JWT_TOKEN,
-    user: {
-      email: userExist.email,
-      subscription: userExist.subscription,
-    },
+
+    email: userExist.email,
   });
 };
 
